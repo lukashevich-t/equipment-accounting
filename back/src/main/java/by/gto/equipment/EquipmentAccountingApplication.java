4078c706e5490a13399d087432b283b292b9b330
Tim Lukashevich <silvernet@yandex.ru>
2023-09-01 10:38:19 +0300

    Add flywaydb migration with initial schema

--- back/src/main/java/by/gto/equipment/EquipmentAccountingApplication.java
+++ back/src/main/java/by/gto/equipment/EquipmentAccountingApplication.java
@@ -1,13 +1,17 @@
 package by.gto.equipment;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EquipmentAccountingApplication {
+    // @PostConstruct
+    // public void init() {
+    //     System.out.println("111");
+    // }
 
-	public static void main(String[] args) {
-		SpringApplication.run(EquipmentAccountingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentAccountingApplication.class, args);
+    }
 }
---
+++ back/src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,246 @@
+-- 
+-- Disable foreign keys
+-- 
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+
+-- 
+-- Set SQL mode
+-- 
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+CREATE TABLE user (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(50) NOT NULL COMMENT 'Имя пользователя (ФИО)',
+  login_cert varchar(100) NOT NULL COMMENT 'Логин пользователя (для входа по сертификату)',
+  login varchar(30) NOT NULL COMMENT 'Логин пользователя (для входа по логину-паролю)',
+  valid bit(1) NOT NULL,
+  password char(64) NOT NULL COMMENT 'пароль, закодированный в sha-1',
+  position varchar(100) NOT NULL COMMENT 'должность',
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 3,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create table `role`
+--
+CREATE TABLE role (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 3,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create table `user_role`
+--
+CREATE TABLE user_role (
+  user_id int(11) NOT NULL,
+  role_id int(11) NOT NULL,
+  PRIMARY KEY (user_id, role_id)
+)
+ENGINE = INNODB,
+AVG_ROW_LENGTH = 4096,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create foreign key
+--
+ALTER TABLE user_role
+ADD CONSTRAINT FK_user_role_role_id FOREIGN KEY (role_id)
+REFERENCES role (id);
+
+--
+-- Create foreign key
+--
+ALTER TABLE user_role
+ADD CONSTRAINT FK_user_role_user_id FOREIGN KEY (user_id)
+REFERENCES user (id);
+
+--
+-- Create table `responsible_person`
+--
+CREATE TABLE responsible_person (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 6,
+AVG_ROW_LENGTH = 3276,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci,
+COMMENT = 'Ответственные лица';
+
+--
+-- Create table `equipment_type`
+--
+CREATE TABLE equipment_type (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 10,
+AVG_ROW_LENGTH = 2730,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci,
+COMMENT = 'Справочник типов объектов инвентаризации';
+
+--
+-- Create index `UK_equipment_type_name` on table `equipment_type`
+--
+ALTER TABLE equipment_type
+ADD UNIQUE INDEX UK_equipment_type_name (name);
+
+--
+-- Create table `equipment_state`
+--
+CREATE TABLE equipment_state (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 10,
+AVG_ROW_LENGTH = 1820,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci,
+COMMENT = 'Справочник состояний объектов инвентаризации';
+
+--
+-- Create table `equipment`
+--
+CREATE TABLE equipment (
+  id binary(16) NOT NULL,
+  type_id int(11) NOT NULL,
+  person_id int(11) NOT NULL,
+  state_id int(11) NOT NULL,
+  comment varchar(1000) DEFAULT NULL,
+  inv_number varchar(20) NOT NULL,
+  purchase_date date DEFAULT NULL,
+  serial varchar(50) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AVG_ROW_LENGTH = 1489,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create foreign key
+--
+ALTER TABLE equipment
+ADD CONSTRAINT FK_equipment_person_id FOREIGN KEY (person_id)
+REFERENCES responsible_person (id);
+
+--
+-- Create foreign key
+--
+ALTER TABLE equipment
+ADD CONSTRAINT FK_equipment_state_id FOREIGN KEY (state_id)
+REFERENCES equipment_state (id);
+
+--
+-- Create foreign key
+--
+ALTER TABLE equipment
+ADD CONSTRAINT FK_equipment_type_id FOREIGN KEY (type_id)
+REFERENCES equipment_type (id);
+
+--
+-- Create table `log`
+--
+CREATE TABLE log (
+  equipment_id binary(16) NOT NULL,
+  action_id tinyint(4) NOT NULL,
+  user_id int(11) NOT NULL,
+  comment varchar(8000) DEFAULT NULL,
+  time datetime DEFAULT NULL
+)
+ENGINE = INNODB,
+AVG_ROW_LENGTH = 2048,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create table `action`
+--
+CREATE TABLE action (
+  id tinyint(4) NOT NULL,
+  name varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AVG_ROW_LENGTH = 3276,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+-- 
+-- Dumping data for table user
+--
+INSERT INTO user VALUES
+(1, 'test', 'CN=test,OU=UIST,O=BTO,L=Minsk,C=BY', 'test', True, '5ee0edb9e2229c0838f1959779f1949031de0123', 'вед.спец.');
+
+-- 
+-- Dumping data for table role
+--
+INSERT INTO role VALUES
+(1, 'user'),
+(2, 'admin');
+
+-- 
+-- Dumping data for table responsible_person
+--
+INSERT INTO responsible_person VALUES
+(1, 'test');
+
+-- 
+-- Dumping data for table equipment_type
+--
+INSERT INTO equipment_type VALUES
+(3, 'МФУ'),
+(2, 'Ноутбук'),
+(1, 'Системный блок');
+
+-- 
+-- Dumping data for table equipment_state
+--
+INSERT INTO equipment_state VALUES
+(1, 'Новый'),
+(2, 'В эксплуатации'),
+(3, 'В резерве'),
+(4, 'В ремонте'),
+(5, 'Списан');
+
+INSERT INTO user_role VALUES
+(1, 1),
+(1, 2);
+
+-- 
+-- Dumping data for table action
+--
+INSERT INTO action VALUES
+(0, 'Нет действия'),
+(1, 'Создание'),
+(2, 'Изменение'),
+(3, 'Обслуживание'),
+(4, 'Списание');
+
+
+-- 
+-- Restore previous SQL mode
+-- 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+
+-- 
+-- Enable foreign keys
+-- 
+/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
